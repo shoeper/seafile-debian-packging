d3b148a9bcb505ef8c70e798cc69786fd1f0a16b
Shuai Lin <linshuai2012@gmail.com>
2016-07-29 15:07:54 +0800

    Updated build script.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV DEBIAN_FRONTEND noninteractive
 RUN echo deb http://ftp.debian.org/debian wheezy-backports main | tee /etc/apt/sources.list.d/wheezy-backports.list && \
     apt-get update -q
 
-RUN apt-get install -y apt-transport-https curl git python tmux vim wget \
+RUN apt-get install -y apt-transport-https curl git python python-dev tmux vim wget \
     autotools-dev cmake debhelper intltool libcurl4-openssl-dev libevent-dev \
     libglib2.0-dev libjansson-dev libqt5webkit5-dev libsqlite3-dev libssl-dev \
     libtool qtbase5-dev qtchooser qttools5-dev qttools5-dev-tools uuid-dev valac
--- build.sh
+++ build.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
 
-if [[ $TRAVIS_TAG != ]]; then
-do
+set -x -e
+
+topdir=/app
+outputdir=/app/debs
+
+mkdir -p $outputdir
+
+cd $topdir
+
+projects=(
+    libsearpc
+    ccnet
+    seafile
+    seafile-client
+)
+
+for project in ${projects[*]}; do
+    builddir=${topdir}/build-${project}
+    mkdir -p $builddir && cd $builddir
+    git clone --branch lpad --depth 1 https://github.com/haiwen/${project}.git
+    cd $project
+    dpkg-buildpackage -b
+
+    cd $builddir
+    # Install the debs because it would be required by latter proejcts (e.g. ccnet requires libsearpc pkgs.)
+    dpkg -i ./*.deb
+    cp ./*.deb $outputdir
+done
+
+if [[ $TRAVIS_TAG == "" ]]; then
+    echo "TRAVIS_TAG not set, skipping upload."
+    exit 0
+fi
+
+for pkg in $outputdir/*.deb; do
+    /app/scripts/bintray-upload-deb $pkg
+done
