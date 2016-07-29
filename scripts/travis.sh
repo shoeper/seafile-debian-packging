b054e3048610cb7d7b2f5953b069c26675e1abe8
Shuai Lin <linshuai2012@gmail.com>
2016-07-29 15:36:33 +0800

    Updated travis scripts.

---
+++ scripts/travis.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x -e
+
+docker pull lins05/seafile-debian-builder
+
+mapfile -t travis_env < <(env |grep TRAVIS)
+docker_envs=""
+for k in ${travis_env[*]}; do
+    docker_envs="$docker_envs -e $k"
+done
+
+docker run --rm -it $docker_envs \
+       -e "BINTRAY_AUTH=$BINTRAY_AUTH" \
+       -v "$(pwd):/app" \
+       --name builder \
+       lins05/seafile-debian-builder \
+       /app/build.sh
